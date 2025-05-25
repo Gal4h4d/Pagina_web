c417563a81b45de84f9fcebf77e59c110692c18b
Gal4h4d <juanditamp@gmail.com>
2025-05-25 01:58:26 -0500

    Guarda cambios antes del pull

--- proyecto_django/settings/local.py
+++ proyecto_django/settings/local.py
@@ -25,3 +25,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
